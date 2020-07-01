1f1598688bd9a42fbefd9f3b67cc3e42b034d234
EduardoAyora <eduardoaayora64@gmail.com>
2020-07-01 11:17:53 -0500

    cambiar esquema

--- authenticate.js
+++ authenticate.js
@@ -2,6 +2,27 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
 
-module.exports = passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,12 @@ const user = require('../controllers/users');
 /* GET users listing. */
 router.get('/login', user.get_login);
 
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/blog',
+                                   failureRedirect: 'login',
+                                   failureFlash: true })
+);
+
 router.get('/signup', user.get_signup);
 
 router.post('/signup', user.post_signup);
