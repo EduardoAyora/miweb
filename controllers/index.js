0ab4f4493a784181cbc297f503979bb19abd866b
EduardoAyora <eduardoaayora64@gmail.com>
2020-07-10 17:59:38 -0500

    empezando a poner en el portafolio los proyectos

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,14 @@
+var Post = require('../models/post');
+
 exports.get_index = function(req, res, next) {
-  res.render('index');
+  Post.findOne({url: 'administrador-de-blog'}, function (err, post) {
+    if (err) {
+      console.log(err);
+      post.sanitizedHtml = 'Hay un problema, no se ha encontrado el proyecto';
+    }
+    if (!post) {
+      post.sanitizedHtml = 'Hay un problema, no se ha encontrado el proyecto';
+    }
+    res.render('index', {post});
+  });
 }
---
+++ models/project.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+const marked = require('marked');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
+const slugify = require('slugify');
+
+var Post = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    sanitizedHtml: {
+      type: String,
+      required: true
+    }
+},{
+    timestamps: true
+});
+
+Post.pre('validate', function (next) {
+  if(this.title) {
+    this.url = slugify(this.title, {
+      lower: true,
+      strict: true
+    })
+  }
+  if(this.content) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.content));
+  }
+  next();
+})
+
+module.exports = mongoose.model('Post', Post);
